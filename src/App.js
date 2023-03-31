ddfeff31a8733361acab307f217fdd35529331c4
veronica365 <naruth10@gmail.com>
2023-03-31 06:23:33 +0300

    Ensure that the components can show when app is loaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoLayout from "./components/hoc";
+import TodoHeader from "./components/ui/TodoHeader";
+import TodoForm from "./components/ui/TodoForm";
+import TodoItem from "./components/ui/TodoItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoLayout>
+      <TodoHeader />
+      <TodoForm />
+      <TodoItem task={{}} />
+    </TodoLayout>
   );
 }
 
