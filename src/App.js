c6da03cc5fe374663cd97feef1a917d62511570a
veronica365 <naruth10@gmail.com>
2023-03-31 06:29:09 +0300

    Display any todos if available

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import Todos from "./logic/todos";
 import TodoLayout from "./components/hoc";
 import TodoHeader from "./components/ui/TodoHeader";
 import TodoForm from "./components/ui/TodoForm";
 import TodoItem from "./components/ui/TodoItem";
 
 function App() {
+  const todos = Todos.listData();
   return (
     <TodoLayout>
       <TodoHeader />
       <TodoForm />
-      <TodoItem task={{}} />
+      {todos.map((task) => (
+        <TodoItem key={task.inde} task={task} />
+      ))}
     </TodoLayout>
   );
 }
